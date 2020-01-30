f27ca75c9b6f3e60b4c37bf4af6de7b02d38a6f6
ivanov.igor <ivanov.igor.it@gmail.com>
2020-01-31 02:11:29 +0300

    Initial Commit

---
+++ simple-wine-v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-wine-v2
+//
+//  Created by Igor Ivanov on 31.01.2020.
+//  Copyright © 2020 Igor Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
