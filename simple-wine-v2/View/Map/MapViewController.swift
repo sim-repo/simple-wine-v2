88a0bcd9df6bfdef7c930367a297c994a57eb388
ivanov.igor <ivanov.igor.it@gmail.com>
2020-02-01 06:15:41 +0300

    selected filter

--- simple-wine-v2/View/Map/MapViewController.swift
+++ simple-wine-v2/View/Map/MapViewController.swift
@@ -1,19 +1,22 @@
 import UIKit
 
 class MapViewController: UIViewController {
-
+    
     @IBOutlet weak var categoryCollectionView: UICollectionView!
     @IBOutlet weak var selectedFilterCollectionView: UICollectionView!
     @IBOutlet weak var filterTableView: UITableView!
     @IBOutlet weak var productTableView: UITableView!
     
+    @IBOutlet weak var selectedFilterHeightConstraint: NSLayoutConstraint!
+    
     var presenter: MapPresenter {
         return MapPresenter.shared
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         presenter.setView(view: self)
         setupTables()
+        selectedFilterHeightConstraint.constant = 1
     }
     
     private func setupTables(){
@@ -35,6 +38,11 @@ extension MapViewController: PresentableView {
     }
     
     func selectedFilterReloadData() {
-        selectedFilterCollectionView.reloadData()
+        if self.presenter.selectedFilterNumberOfRowsInSection() > 0 {
+            self.selectedFilterHeightConstraint.constant = 60
+        } else {
+            self.selectedFilterHeightConstraint.constant = 1
+        }
+        self.selectedFilterCollectionView.reloadData()
     }
 }
